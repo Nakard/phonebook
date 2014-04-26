742850310a243971c404518124f83d7ec1d11848
Arkadiusz Moskwa <a.moskwa@gmail.com>
2014-04-26 12:31:39 +0200

    js listener change

---
+++ controllers/EditController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * EditController.php
+ *
+ * Creation date: 2014-04-26
+ * Creation time: 12:28
+ *
+ * @author Arkadiusz Moskwa <a.moskwa@gmail.com>
+ */
+
+class EditController {
+
+} 
