85e4ca8a8c3492c1231fa090900d805a45de4dd1
Arkadiusz Moskwa <a.moskwa@gmail.com>
2014-04-26 12:41:30 +0200

    setting up routes

--- controllers/EditController.php
+++ controllers/EditController.php
@@ -8,6 +8,32 @@
  * @author Arkadiusz Moskwa <a.moskwa@gmail.com>
  */
 
-class EditController {
+class Phonebook_EditController extends Zend_Controller_Action {
+
+    protected $entityManager;
+
+    public function init()
+    {
+        $registry = Zend_Registry::getInstance();
+        $this->entityManager = $registry->entityManager;
+        /**
+         * @var Zend_Controller_Action_Helper_ContextSwitch $contextSwitch
+         */
+        $contextSwitch = $this->_helper->getHelper('contextSwitch');
+        $contextSwitch
+            ->setActionContext('person', 'json')
+            ->setActionContext('number', 'json')
+            ->initContext();
+    }
+
+    public function personAction()
+    {
+
+    }
+
+    public function numberAction()
+    {
+
+    }
 
 } 
